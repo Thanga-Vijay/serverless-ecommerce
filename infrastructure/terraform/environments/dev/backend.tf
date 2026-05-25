e1be7210fe983c9baee296a076e51fe2716d602a
Vijay <vijay@gmail.com>
2026-05-25 17:18:55 +0530

    update bucket region

--- infrastructure/terraform/environments/dev/backend.tf
+++ infrastructure/terraform/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket  = "tf-state-bucket-sls"
-    key     = "serverless-ecommerce/dev/terraform.tfstate"
-    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    region  = "ap-south-1"
     encrypt = true
   }
 }
